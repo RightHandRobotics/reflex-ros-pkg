c232dbd33b6c0a9808ed2f3fbf9a6880da68639b
Eric Schneider <franz.eric.schneider@gmail.com>
2015-08-05 14:23:42 -0400

    Hand will stop on contact now when tactile_stops is enabled

--- reflex/src/reflex/motor.py
+++ reflex/src/reflex/motor.py
@@ -18,8 +18,10 @@ class Motor(object):
         self.motor_msg = reflex_msgs.msg.Motor()
         self.motor_cmd = 0.0
         self.speed = 0.0
-        self.in_control_torque_mode = False
-        self.torque_cmd = 0.0
+        self.finger = None
+        self.tactile_stops_enabled = False
+        self.in_control_force_mode = False
+        self.force_cmd = 0.0
         self.previous_load_control_output = 0.0
         self.previous_load_control_error = 0.0
         self.position_update_occurred = False
@@ -88,22 +90,22 @@ class Motor(object):
         elif goal_vel <= 0.0:
             self.set_motor_angle(0.0)
 
-    def enable_torque_control(self):
-        self.in_control_torque_mode = True
+    def enable_force_control(self):
+        self.in_control_force_mode = True
         self.previous_load_control_output = self.get_current_joint_angle()
         self.previous_load_control_error = 0.0
 
-    def disable_torque_control(self):
-        self.in_control_torque_mode = False
+    def disable_force_control(self):
+        self.in_control_force_mode = False
 
-    def set_torque_cmd(self, torque_cmd):
+    def set_force_cmd(self, force_cmd):
         '''
         Bounds the given goal load and sets it as the goal
         '''
-        self.torque_cmd = min(max(torque_cmd, 0.0), self.OVERLOAD_THRESHOLD)
+        self.force_cmd = min(max(force_cmd, 0.0), self.OVERLOAD_THRESHOLD)
 
-    def control_torque(self, current_torque):
-        current_error = self.torque_cmd - current_torque
+    def control_force(self, current_force):
+        current_error = self.force_cmd - current_force
         k = 16e-4 * 0.025  # Compensator gain - higher gain has faster response and is more unstable
                             # The 0.025 accounts for a 20Hz update rate
         output = self.previous_load_control_output + k * (current_error + self.previous_load_control_error)
@@ -111,13 +113,28 @@ class Motor(object):
         self.previous_load_control_output = output
         self.previous_load_control_error = current_error
 
+    def disable_tactile_stops(self):
+        self.tactile_stops_enabled = False
+
+    def enable_tactile_stops(self):
+        self.tactile_stops_enabled = True
+
     def loosen_if_overloaded(self, load):
         '''
         Takes the given load and checks against threshold, loosen motor if over
         '''
         if abs(load) > self.OVERLOAD_THRESHOLD:
-            rospy.logwarn("Motor %s overloaded at %f, loosening" % (self.name, load))
+            rospy.logwarn("Motor %s overloaded at %f, loosening", self.name, load)
             self.loosen()
+
+    def loosen_if_in_contact(self):
+        '''
+        Takes the finger tactile data, loosens motor if in contact
+        '''
+        tolerance = 0.001
+        if self.finger.is_finger_in_contact() and (self.motor_cmd > self.motor_msg.joint_angle + tolerance):
+            rospy.logdebug("Motor %s in contact", self.name)
+            self.loosen(0)
 
     def tighten(self, tighten_angle=0.05):
         '''
@@ -137,7 +154,8 @@ class Motor(object):
         self.handle_motor_load(data.load)
 
     def handle_motor_load(self, load):
-        if self.in_control_torque_mode:
-            self.control_torque(load)
+        if self.in_control_force_mode:
+            self.control_force(load)
+        elif self.finger and self.tactile_stops_enabled:
+            self.loosen_if_in_contact()
         self.loosen_if_overloaded(load)
-
--- reflex/src/reflex/reflex_takktile_hand.py
+++ reflex/src/reflex/reflex_takktile_hand.py
@@ -24,52 +24,91 @@ class ReflexTakktileHand():
         self.fingers = {self.namespace + '_f1': finger.Finger(),
                         self.namespace + '_f2': finger.Finger(),
                         self.namespace + '_f3': finger.Finger()}
+        self.connect_motors_to_fingers()
+        self.set_speed_service = rospy.ServiceProxy(self.namespace + '/set_speed', reflex_msgs.srv.SetSpeed)
+        self.calibrate_fingers_service = rospy.ServiceProxy(self.namespace + '/calibrate_fingers', Empty)
+        self.calibrate_tactile_service = rospy.ServiceProxy(self.namespace + '/calibrate_tactile', Empty)
         rospy.Subscriber(self.namespace + '/command',
                          reflex_msgs.msg.ReflexCommand, self.receive_cmd_cb)
         rospy.Subscriber(self.namespace + '/command_position',
                          reflex_msgs.msg.PoseCommand, self.receive_angle_cmd_cb)
         rospy.Subscriber(self.namespace + '/command_velocity',
                          reflex_msgs.msg.VelocityCommand, self.receive_vel_cmd_cb)
-        rospy.Subscriber(self.namespace + '/command_motor_torque',
-                         reflex_msgs.msg.TorqueCommand, self.receive_torque_cmd_cb)
+        rospy.Subscriber(self.namespace + '/command_motor_force',
+                         reflex_msgs.msg.ForceCommand, self.receive_force_cmd_cb)
         rospy.Subscriber(self.namespace + '/hand_state',
                          reflex_msgs.msg.Hand, self.receive_hand_state_cb)
-        self.set_speed_service = rospy.ServiceProxy(self.namespace + '/set_speed', reflex_msgs.srv.SetSpeed)
-        self.calibrate_fingers_service = rospy.ServiceProxy(self.namespace + '/calibrate_fingers', Empty)
-        self.calibrate_tactile_service = rospy.ServiceProxy(self.namespace + '/calibrate_tactile', Empty)
+        rospy.Service(self.namespace + '/enable_tactile_stops', Empty, self.enable_tactile_stops)
+        rospy.Service(self.namespace + '/disable_tactile_stops', Empty, self.disable_tactile_stops)
+        self.tactile_stops_enabled = False
         self.comms_timeout = 5.0  # Seconds with no communications until hand stops
         self.latest_update = rospy.get_rostime()
         rospy.loginfo('ReFlex Takktile hand has started, waiting for commands...')
 
+    def connect_motors_to_fingers(self):
+        for ID, motor in self.motors.items():
+            if ID != self.namespace + '_preshape':
+                motor.finger = self.fingers[ID]
+
     def receive_cmd_cb(self, data):
-        self.disable_torque_control()
+        reset = self.tactile_stops_enabled
+        if reset:
+            self.disable_tactile_stops()
+        self.disable_force_control()
         self.set_speeds(data.velocity)
         self.set_angles(data.pose)
+        if reset:
+            self.enable_tactile_stops()
 
     def receive_angle_cmd_cb(self, data):
-        self.disable_torque_control()
+        reset = self.tactile_stops_enabled
+        if reset:
+            self.disable_tactile_stops()
+        self.disable_force_control()
         self.reset_speeds()
         self.set_angles(data)
+        if reset:
+            self.enable_tactile_stops()
 
     def receive_vel_cmd_cb(self, data):
-        self.disable_torque_control()
+        reset = self.tactile_stops_enabled
+        if reset:
+            self.disable_tactile_stops()
+        self.disable_force_control()
         self.set_velocities(data)
+        if reset:
+            self.enable_tactile_stops()
 
-    def receive_torque_cmd_cb(self, data):
-        self.disable_torque_control()
+    def receive_force_cmd_cb(self, data):
+        self.disable_tactile_stops()
+        self.disable_force_control()
         self.reset_speeds()
-        self.set_torque_cmds(data)
-        self.enable_torque_control()
+        self.set_force_cmds(data)
+        self.enable_force_control()
 
     def receive_hand_state_cb(self, data):
         self.latest_update = rospy.get_rostime()
-        self.motors['/reflex_takktile_f1'].receive_state_cb(data.motor[0])
-        self.motors['/reflex_takktile_f2'].receive_state_cb(data.motor[1])
-        self.motors['/reflex_takktile_f3'].receive_state_cb(data.motor[2])
-        self.motors['/reflex_takktile_preshape'].receive_state_cb(data.motor[3])
-        self.fingers['/reflex_takktile_f1'].receive_state_cb(data.finger[0])
-        self.fingers['/reflex_takktile_f2'].receive_state_cb(data.finger[1])
-        self.fingers['/reflex_takktile_f3'].receive_state_cb(data.finger[2])
+        self.motors[self.namespace + '_f1'].receive_state_cb(data.motor[0])
+        self.motors[self.namespace + '_f2'].receive_state_cb(data.motor[1])
+        self.motors[self.namespace + '_f3'].receive_state_cb(data.motor[2])
+        self.motors[self.namespace + '_preshape'].receive_state_cb(data.motor[3])
+        self.fingers[self.namespace + '_f1'].receive_state_cb(data.finger[0])
+        self.fingers[self.namespace + '_f2'].receive_state_cb(data.finger[1])
+        self.fingers[self.namespace + '_f3'].receive_state_cb(data.finger[2])
+
+    def disable_tactile_stops(self, data=None):
+        self.tactile_stops_enabled = False
+        for ID, motor in self.motors.items():
+            motor.disable_tactile_stops()
+        rospy.sleep(0.05)  # Lets commands stop before allowing any other actions
+        return []
+
+    def enable_tactile_stops(self, data=None):
+        rospy.sleep(0.07)  # Lets other actions happen before beginning constant commands
+        self.tactile_stops_enabled = True
+        for ID, motor in self.motors.items():
+            motor.enable_tactile_stops()
+        return []
 
     def set_angles(self, pose):
         self.motors[self.namespace + '_f1'].set_motor_angle(pose.f1)
@@ -89,25 +128,25 @@ class ReflexTakktileHand():
         self.motors[self.namespace + '_f3'].set_motor_speed(speed.f3)
         self.motors[self.namespace + '_preshape'].set_motor_speed(speed.preshape)
 
-    def set_torque_cmds(self, torque):
-        self.motors[self.namespace + '_f1'].set_torque_cmd(torque.f1)
-        self.motors[self.namespace + '_f2'].set_torque_cmd(torque.f2)
-        self.motors[self.namespace + '_f3'].set_torque_cmd(torque.f3)
-        self.motors[self.namespace + '_preshape'].set_torque_cmd(torque.preshape)
+    def set_force_cmds(self, torque):
+        self.motors[self.namespace + '_f1'].set_force_cmd(torque.f1)
+        self.motors[self.namespace + '_f2'].set_force_cmd(torque.f2)
+        self.motors[self.namespace + '_f3'].set_force_cmd(torque.f3)
+        self.motors[self.namespace + '_preshape'].set_force_cmd(torque.preshape)
 
     def reset_speeds(self):
         for ID, motor in self.motors.items():
             motor.reset_motor_speed()
 
-    def disable_torque_control(self):
+    def disable_force_control(self):
         for ID, motor in self.motors.items():
-            motor.disable_torque_control()
+            motor.disable_force_control()
         rospy.sleep(0.05)  # Lets commands stop before allowing any other actions
 
-    def enable_torque_control(self):
+    def enable_force_control(self):
         rospy.sleep(0.05)  # Lets other actions happen before beginning constant torque commands
         for ID, motor in self.motors.items():
-            motor.enable_torque_control()
+            motor.enable_force_control()
 
     def calibrate_fingers(self):
         self.calibrate_fingers_service()
