2a31698bf21e5a395e715b318b803cb6075220cd
Alyssa Gang <aegang817@gmail.com>
2017-06-20 15:37:55 -0400

    Updated comments to be more accurate

--- firmware/reflex-firmware-master/state.h
+++ firmware/reflex-firmware-master/state.h
@@ -4,28 +4,31 @@
 #include <stdint.h>
 #include "reflex.h"
 
+//state_t 
+//struct to save the state of the hand in a format to be sent to the computer
 typedef struct
-{
-  uint8_t  header[6];                          // 0-3
-  uint32_t systime;                            // 4-7
-  uint16_t takktile_pressures[NUM_SENSORS];    // 8-83
-  uint16_t takktile_temperatures[NUM_SENSORS]; // 84-159
-  uint16_t encoders[NUM_ENC];                  // 160-165
-  uint8_t  dynamixel_error_status[4];          // 166-169
-  uint16_t dynamixel_angles[4];                // 170-177
-  uint16_t dynamixel_speeds[4];
-  uint16_t dynamixel_loads[4];
-  uint8_t  dynamixel_voltages[4];
-  uint8_t  dynamixel_temperatures[4];
-  uint16_t imus[NUM_IMUS*4];                   // 160-165
-} __attribute__((packed)) state_t;
+{                                               // Bytes in Packet
+  uint8_t  header[6];                           // 0-5
+  uint32_t systime;                             // 6-9
+  uint16_t takktile_pressures[NUM_SENSORS];     // 10-115
+  uint16_t takktile_temperatures[NUM_SENSORS];  // 116-221
+  uint16_t encoders[NUM_ENC];                   // 222-227
+  uint8_t  dynamixel_error_status[4];           // 228-231
+  uint16_t dynamixel_angles[4];                 // 232-239
+  uint16_t dynamixel_speeds[4];                 // 240-247
+  uint16_t dynamixel_loads[4];                  // 248-255
+  uint8_t  dynamixel_voltages[4];               // 256-259
+  uint8_t  dynamixel_temperatures[4];           // 260-263
+  uint16_t imus[NUM_IMUS*4];                    // 264-295
 
+//status_t
+//struct used to keep track of what is working or not working
 typedef struct
 {
-  uint8_t finger[NUM_FINGERS];                 // 0-2
-  uint8_t takktileSensor[NUM_SENSORS];         // 3-11
-  uint8_t encoders[NUM_ENC];                   // 12-14
-  uint8_t imus[NUM_IMUS];
+  uint8_t finger[NUM_FINGERS];                 // Status of 3 fingers
+  uint8_t takktileSensor[NUM_SENSORS];         // Status of 53 sensors
+  uint8_t encoders[NUM_ENC];                   // Status of 3 encoders
+  uint8_t imus[NUM_IMUS];                      // Status of 4 IMUs
 } __attribute__((packed)) status_t;
 
 // Values meaning:
@@ -47,4 +50,3 @@ extern volatile status_t handStatus;
 void state_init();
 
 #endif
-
