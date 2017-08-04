39ed1aedaecf928d0b6d6542f9e302deabbb4aec
Alyssa Gang <aegang817@gmail.com>
2017-08-04 14:22:43 -0400

    Updated hand state on ROS side to match new firmware packet with IMU calibration values

--- reflex_driver/reflex_hand.h
+++ reflex_driver/reflex_hand.h
@@ -44,6 +44,8 @@ namespace reflex_hand
     uint8_t  dynamixel_voltages_[4];
     uint8_t  dynamixel_temperatures_[4];
     int16_t  imus[NUM_IMUS*4];
+    int8_t  imu_calibration_status[NUM_IMUS];
+    uint16_t imu_calibration_data[NUM_IMUS*22];
     ReflexHandState();
   };
 
