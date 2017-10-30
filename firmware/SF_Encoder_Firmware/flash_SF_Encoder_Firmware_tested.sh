d44a2bd9476eafd788795407392b3983d3bfbe15
Lance <lancewbantoto@gmail.com>
2017-10-30 14:27:29 -0400

    Add echo success message

--- firmware/SF_Encoder_Firmware/flash_SF_Encoder_Firmware_tested.sh
+++ firmware/SF_Encoder_Firmware/flash_SF_Encoder_Firmware_tested.sh
@@ -4,3 +4,5 @@ avrdude -c avrispmkII -p atmega328p -U lfuse:w:0xFF:m
 avrdude -c avrispmkII -p atmega328p -U hfuse:w:0xD9:m
 avrdude -c avrispmkII -p atmega328p -U efuse:w:0x07:m
 avrdude -c avrispmkII -p atmega328p -U flash:w:"./SF_Encoder_Firmware.hex":i
+
+echo "Flashed firmware successfully!"
