344e3a035dc420fd9beb5245aadfe93eb2eaee1c
Kevin Lam <kvnlam23@gmail.com>
2018-02-15 17:23:50 -0500

    Reorganized driver node header functions

--- reflex_driver2/src/reflex_driver_node.h
+++ reflex_driver2/src/reflex_driver_node.h
@@ -46,53 +46,103 @@
 
 #define CALIBRATION_ERROR 0.05  // Encoder delta signifying movement in calibration
 
+//Setup Functions
 void signal_handler(int signum);
+
 void load_params(ros::NodeHandle nh);
+
+
+//Callback Functions
 void receive_raw_cmd_cb(reflex_hand::ReflexHand *rh,
                         const reflex_msgs2::RawServoCommands::ConstPtr &msg);
+
 void receive_angle_cmd_cb(reflex_hand::ReflexHand *rh,
                           const reflex_msgs2::RadianServoCommands::ConstPtr &msg);
+
+void reflex_hand_state_cb(const reflex_hand::ReflexHandState * const state);
+
+
+//Service Functions
+bool calibrate_tactile(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
+
+bool calibrate_fingers(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
+
+
+//Motor Functions
 uint16_t pos_rad_to_raw(float rad_command, int motor_idx);
+
 bool set_motor_speed(reflex_hand::ReflexHand *rh,
 					 reflex_msgs2::SetSpeed::Request &req, reflex_msgs2::SetSpeed::Response &res);
+
 uint16_t speed_rad_to_raw(float rad_per_s_command, int motor_idx);
+
 void check_for_potential_motor_wraps_and_rezero();
+
 bool enable_torque(reflex_hand::ReflexHand *rh, std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
+
 bool disable_torque(reflex_hand::ReflexHand *rh, std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
-bool calibrate_tactile(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
-bool calibrate_fingers(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
-void populate_tactile_threshold(int threshold);
-bool set_tactile_threshold(reflex_msgs2::SetTactileThreshold::Request &req,
-                           reflex_msgs2::SetTactileThreshold::Response &res);
-int pressure_offset(int finger, int sensor);
-int update_encoder_offset(int raw_value, int last_value, int current_offset);
-float calc_proximal_angle(int raw_enc_value, int offset, double zero);
-float calc_motor_angle(int inversion, int raw_dyn_value, double ratio, double zero);
-float calc_distal_angle(float spool, float proximal);
-int calc_pressure(const reflex_hand::ReflexHandState* const state, int finger, int sensor);
-int calc_contact(reflex_msgs2::Hand hand_msg, int finger, int sensor);
-void reflex_hand_state_cb(const reflex_hand::ReflexHandState * const state);
-void calibrate_tactile_sensors(const reflex_hand::ReflexHandState* const state, reflex_msgs2::Hand hand_msg);
-void log_current_tactile_locally(const reflex_hand::ReflexHandState* const state);
-void log_current_tactile_to_file(const reflex_hand::ReflexHandState* const state, int finger);
-void calibrate_encoders_locally(const reflex_hand::ReflexHandState* const state);
 
-void log_imu_calibration_data(const reflex_hand::ReflexHandState* const state);
-void log_current_imu_offsets_to_file(const reflex_hand::ReflexHandState* const state, int finger);
-
-bool check_for_finger_movement(const reflex_hand::ReflexHandState* const state);
 void move_fingers_in(const reflex_hand::ReflexHandState* const state);
+
+float calc_motor_angle(int inversion, int raw_dyn_value, double ratio, double zero);
+
 void calibrate_motors_locally(const reflex_hand::ReflexHandState* const state);
-void log_encoder_zero_to_file();
-void log_motor_zero_to_file_and_close();
+
 void populate_motor_state(reflex_msgs2::Hand* hand_msg, const reflex_hand::ReflexHandState* const state);
+
 float load_raw_to_signed(int load, int motor_idx);
 
+
+//Tactile Functions
+void populate_tactile_threshold(int threshold);
+
+bool set_tactile_threshold(reflex_msgs2::SetTactileThreshold::Request &req,
+                           reflex_msgs2::SetTactileThreshold::Response &res);
+
+int pressure_offset(int finger, int sensor);
+
+void calibrate_tactile_sensors(const reflex_hand::ReflexHandState* const state, reflex_msgs2::Hand hand_msg);
+
+bool check_for_finger_movement(const reflex_hand::ReflexHandState* const state);
+
+int calc_pressure(const reflex_hand::ReflexHandState* const state, int finger, int sensor);
+
+int calc_contact(reflex_msgs2::Hand hand_msg, int finger, int sensor);
+
+
+//Encoder Functions
+int update_encoder_offset(int raw_value, int last_value, int current_offset);
+
+void calibrate_encoders_locally(const reflex_hand::ReflexHandState* const state);
+
+float calc_proximal_angle(int raw_enc_value, int offset, double zero);
+
+float calc_distal_angle(float spool, float proximal);
+
+
+//Logger Functions
+void log_current_tactile_locally(const reflex_hand::ReflexHandState* const state);
+
+void log_current_tactile_to_file(const reflex_hand::ReflexHandState* const state, int finger);
+
+void log_imu_calibration_data(const reflex_hand::ReflexHandState* const state);
+
+void log_current_imu_offsets_to_file(const reflex_hand::ReflexHandState* const state, int finger);
+
+void log_encoder_zero_to_file();
+
+void log_motor_zero_to_file_and_close();
+
+
+//IMU Functions
 bool initIMUCal(reflex_hand::ReflexHand *rh, std_srvs::Empty::Request &req, 
-                    std_srvs::Empty::Response &res);	
+                    std_srvs::Empty::Response &res);
+
 bool saveIMUCalData(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
+
 bool loadIMUCalData(reflex_hand::ReflexHand *rh, std_srvs::Empty::Request &req, 
                     std_srvs::Empty::Response &res);
+
 bool refreshIMUCalData(reflex_hand::ReflexHand *rh, std_srvs::Empty::Request &req, 
                     std_srvs::Empty::Response &res);
 
